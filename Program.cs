f65c5af461bb0c656ca20916b93e3f1ff4552256
Rania-18 <112584626+Rania-18@users.noreply.github.com>
2022-12-23 03:24:43 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {static bool prime(int num)
+        {for (int i = 2; i < num; i++)
+                if (num % i == 0) return false;
+            return true;
+   }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter the first number");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter the sesond numer");
+            int n2 = int.Parse(Console.ReadLine());
+            for (int n1 = y; n1 < n2; ++n1)
+            { if(prime(n1))       
+                    Console.WriteLine(n1);
+
+            }
+            Console.ReadKey();
+        
+           
+            
+        }
+    }
+}
---
+++ project 2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+        
+    {
+        static bool perfect(int num)
+        {
+            int sum = 0;
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                    sum = sum + i;
+            }
+            if (sum == num) return true;
+            return false;
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter the first number");
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter the second number");
+            int n2 = int.Parse(Console.ReadLine());
+            for (int n1 = i; n1 <= n2; n1++)
+                if (perfect(n1)) Console.WriteLine(n1)  ;
+            Console.ReadKey();
+
+        }
+    }
+}
